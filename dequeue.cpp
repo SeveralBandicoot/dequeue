e829fe1c4f95f2b5c97e0a57122dfe8fa6599905
technicalace <arguelloandrew15@gmail.com>
2024-11-14 14:51:09 -0600

    final commit

---
+++ dequeue.cpp
@@ -0,0 +1,59 @@
+/*
+
+Dequeue
+
+11/10/24
+
+@AJ Enrique Arguello
+
+Objectives: 
+    What is dequeue? When we pop( ) the Queue, where does it remove the data from?
+
+    Create an example with Strings and load 4 names into a Queue and a Stack.
+
+    Run a Display Function( )
+
+    pop each one and display again
+
+    Compare your results
+
+*/
+
+#include <iostream>
+#include <deque>
+#include <stack>
+
+using namespace std;
+
+void display(deque<string> dequeStrings, stack<string> stackStrings);
+int main() {
+    deque<string> dequeStrings = {"Abigal", "Bailey", "Sam", "Jason", "Norman"};
+    stack<string> stackStrings;
+
+    // push numbers to stack
+    stackStrings.push("Abigal");
+    stackStrings.push("Bailey");
+    stackStrings.push("Sam");
+    stackStrings.push("Jason");
+    stackStrings.push("Norman");
+
+    /*dequeStrings.pop_back();
+    stackStrings.pop();*/
+
+    display(dequeStrings, stackStrings);
+
+    return 0;
+}
+
+void display(deque<string> dequeStrings, stack<string> stackStrings) {
+    cout << "Queue Display: " << endl; // display contents of queue
+    for (int i = 0; i < dequeStrings.size(); i ++) {
+        cout << dequeStrings[i] << endl;
+    }
+
+    cout << "\n\nStack Display" << endl; // display contents of stack
+    while (!stackStrings.empty()) {
+        cout << stackStrings.top() << endl;
+        stackStrings.pop();
+    }
+}
